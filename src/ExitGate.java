2aac104338fe9269fe64576ff39527457b63a338
Shimeer <shimeerh@gmail.com>
2019-08-05 02:53:08 +0530

    Final jar file

--- src/ExitGate.java
+++ src/ExitGate.java
@@ -1,13 +1,11 @@
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
+import javax.swing.JOptionPane;		// to display message
 
 import java.awt.Font;
 import javax.swing.JTextField;
@@ -20,7 +18,17 @@ public class ExitGate extends JFrame {
 	private JPanel contentPane;
 	private JTextField licencetext;
 	private JTextField textFieldIn;
-
+	private JLabel LabelIn;
+	private JLabel LicenceNolb;
+	private JButton showB;
+	private JButton exitB;
+	private JLabel outlbl;
+	private JLabel dur;
+	private JTextField txtdur;
+	private JLabel feeLb;
+	private JLabel lbhead ;
+	private JTextField txtfee;
+	private JTextField outtime;
 
 	public ExitGate() {
 		super("Exit");
@@ -28,17 +36,17 @@ public class ExitGate extends JFrame {
 		setBounds(100, 100, 700, 473);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(0, 51, 51));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton showB = new JButton("show");
+		//generate reciept
+		showB = new JButton("show");
 		showB.setBackground(new Color(204, 255, 255));
 		showB.setBounds(321, 151, 90, 25);
 		showB.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
 		contentPane.add(showB);
 		
-		JButton exitB = new JButton("Exit");
+		exitB = new JButton("Exit");
 		exitB.setBackground(new Color(204, 255, 255));
 		exitB.setBounds(314, 388, 97, 25);
 		exitB.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
@@ -46,63 +54,71 @@ public class ExitGate extends JFrame {
 		contentPane.add(exitB);
 		
 		
-		
-		JLabel LicenceNolb = new JLabel("Licence Number");
+		LicenceNolb = new JLabel("Licence Number");
 		LicenceNolb.setForeground(new Color(255, 255, 255));
 		LicenceNolb.setFont(new Font("Stencil", Font.PLAIN, 15));
 		LicenceNolb.setBounds(200, 103, 148, 35);
 		contentPane.add(LicenceNolb);
 		
+		//input for licence number
 		licencetext= new JTextField("Enter here");
 		licencetext.setBounds(386, 107, 116, 22);
 		contentPane.add(licencetext);
 		licencetext.setColumns(10);
 		
-		JLabel LabelIn = new JLabel("In-Time");
+		LabelIn = new JLabel("In-Time");
 		LabelIn.setForeground(new Color(255, 255, 255));
 		 LabelIn.setFont(new Font("Stencil", Font.PLAIN, 15));
 		 LabelIn.setBounds(53, 226, 85, 16);
 		contentPane.add( LabelIn);
 		
+		//showing IN Time 
 		textFieldIn = new JTextField();
 		textFieldIn.setBounds(186, 221, 116, 22);
 		contentPane.add(textFieldIn);
 		textFieldIn.setColumns(10);
+		textFieldIn.setEditable(false);
 		
-		JLabel outlbl = new JLabel("Out-Time");
+		outlbl = new JLabel("Out-Time");
 		outlbl.setForeground(new Color(255, 255, 255));
 		outlbl.setFont(new Font("Stencil", Font.PLAIN, 15));
 		outlbl.setBounds(417, 226, 85, 16);
 		contentPane.add(outlbl);
 		
-		JTextField outtime= new JTextField();
+		//show out time
+		outtime= new JTextField();
 		outtime.setBounds(539, 221, 116, 22);
 		contentPane.add(outtime);
 		outtime.setColumns(10);
+		outtime.setEditable(false);
 		
-		JLabel dur= new JLabel("Duration");
+		dur= new JLabel("Duration");
 		dur.setForeground(new Color(255, 255, 255));
 		dur.setFont(new Font("Stencil", Font.PLAIN, 15));
 		dur.setBounds(53, 319, 85, 16);
 		contentPane.add(dur);
 		
-		JTextField txtdur= new JTextField();
+		//show parking duration
+		txtdur= new JTextField();
 		txtdur.setBounds(186, 314, 116, 22);
 		contentPane.add(txtdur);
 		txtdur.setColumns(10);
+		txtdur.setEditable(false);
 		
-		JLabel feeLb= new JLabel("Fee");
+		feeLb= new JLabel("Fee");
 		feeLb.setForeground(new Color(255, 255, 255));
 		feeLb.setFont(new Font("Stencil", Font.PLAIN, 15));
 		feeLb.setBounds(417, 319, 56, 16);
 		contentPane.add(feeLb);
 		
-		JTextField txtfee= new JTextField();
+		//show parking amount
+		txtfee= new JTextField();
 		txtfee.setBounds(539, 316, 116, 22);
 		contentPane.add(txtfee);
 		txtfee.setColumns(10);
+		txtfee.setEditable(false);
 		
-		JLabel lbhead = new JLabel("Parking Fee Generator");
+		lbhead = new JLabel("Parking Fee Generator");
 		lbhead.setFont(new Font("Tahoma", Font.PLAIN, 25));
 		lbhead.setForeground(Color.WHITE);
 		lbhead.setBounds(237, 13, 258, 35);
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.io.File;
+
 import javax.swing.JFrame;
 
 public class Main {
@@ -12,25 +14,36 @@ public class Main {
 		eg.setSize(800,500);
 		eg.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		for(int i=1; i<=4; i++) {
+		//directory of images
+		File Files = new File("C:\\Users\\shime\\eclipse-workspace\\ParkingSpaceAnalyser\\sample images");
+		//count number of images(cars)
+		int Count = Files.list().length;
+//		System.out.println(Count);
+		
+		for(int i=1; i<=Count; i++) {
+			//location for image file
+			
 			String s = ob.getText("C:\\Users\\shime\\eclipse-workspace\\ParkingSpaceAnalyser\\sample images\\" + i + ".jpeg");
 			if(s == null) {
 				continue;
 			}
 			s = s.replaceAll("[^a-zA-Z0-9_-]", "");
 			System.out.println(s);
+			//insert values into table
 			int status = db.entry(s);
 			if(status == 1) {
 				System.out.println("Inserted");
 			}
+			//get data from table
 			String rowData[][] = db.garage();
 //			System.out.println(rowData.length);
-			Thread.sleep(2000);
+			//insert row to table
 			eg.model.addRow(rowData[i-1]);
 			eg.count++;
 			eg.vacantSpace--;
 			eg.countLb.setText("Filled->" + Integer.toString( eg.count));
 			eg.vacantLb.setText("Vacant->" + Integer.toString( eg.vacantSpace));
+			Thread.sleep(2000);
 			
 		}
 	}
